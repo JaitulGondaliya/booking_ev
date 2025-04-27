4e978f56b46f0b14224c6ab31338133b645f8a2e
JaitulGondaliya <jaitulgondaliya5993@gmail.com>
2025-04-27 11:59:42 +0530

    Commit.....

---
+++ lib/CommonWidget/APIConst.dart
@@ -0,0 +1,5 @@
+class Apiconst {
+    static const String base_url = "http://vehiclerental2025.hstn.me/API/";
+    static const String cookie = "__test=9f47acd4b6983f1dfa648e69bcc71dbb";
+    static const String user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36";
+}
